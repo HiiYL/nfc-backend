bd31a786a0eff292d6f400935beca6668f28d680
= <=>
2014-11-12 16:30:47 +0800

    Now only checks for updated data based on last database update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
 	scope :sorted_by_status, -> {order(:status)}
 	scope :num_of_checkins, ->{where(status: "used").count}
+	scope :db_last_updated, ->{order(:updated_at).limit(1).first}
 end

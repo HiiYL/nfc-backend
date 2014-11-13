d4b6a0cace37c70744b4d07f0f6e5b69c925ffee
= <=>
2014-11-13 15:20:42 +0800

    Add AJAX refresh

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,43 +2,35 @@ require 'json'
 require 'open-uri'
 class StaticPagesController < ApplicationController
   def home
-    @users = User.all
+    if session[:test].nil?
+      @users = User.all
+    else
+      @users =User.all.send(session[:test])
+    end
     @count = User.all.num_of_checkins
   end
   def sort_by_changed_date
-    @users = User.all.last_updated
-    render 'home'
+    session[:test] = "last_updated"
+    @users = User.all.send(session[:test])
+    respond_to do |format|
+      format.html {render'home'}
+      format.js {}
+    end
   end
   def sort_by_status
-    @users = User.all.sorted_by_status
-    render 'home'
-  end
-  def refresh_db
-    EventbriteAPI::Configuration.access_token="54X654CD2AJ3XPAPM45B"
-    #EventbriteAPI::Configuration.access_token="***REMOVED***"
-    @eb = EventbriteAPI.new
-    #@event = @eb.events(id:***REMOVED***)
-    @event = @eb.events(id:***REMOVED***)
-    page_count = @event.attendees(status:"attending",changed_since:"2010-11-12T7:23:03Z").get.body["pagination"]["page_count"]
-    for i in 1..page_count
-      @attendees = @event.attendees(status:"attending",page:i,changed_since:"2010-11-12T7:23:03Z").get.body["attendees"]
-      @attendees.each do |test| 
-        unless User.find_by(name: test["profile"]["name"].to_s)
-          user = User.new
-          user.name = test["profile"]["name"]
-          user.email = test["profile"]["email"]
-          user.status = test["barcodes"].first["status"]
-          user.save
-        end
-      end
+    session[:test] = "sorted_by_status"
+    @users = User.all.send(session[:test])
+    respond_to do |format|
+      format.html {render'home'}
+      format.js {}
     end
   end
   def update_db
-    EventbriteAPI::Configuration.access_token="***REMOVED***"
     #EventbriteAPI::Configuration.access_token="***REMOVED***"
+    EventbriteAPI::Configuration.access_token="***REMOVED***"
     @eb = EventbriteAPI.new
-    #@event = @eb.events(id:***REMOVED***)
     @event = @eb.events(id:***REMOVED***)
+    #@event = @eb.events(id:***REMOVED***)
     if User.count > 0
       @time = User.last_updated.first.updated_at.utc
       @time = @time.strftime("%Y-%m-%dT%H:%M:%SZ")
@@ -65,26 +57,11 @@ class StaticPagesController < ApplicationController
       end
     end 
   end
-  def ajax_verify
-    user = User.first
-    respond_to do |format|
-      format.js
-    end
-  end
   def ajax
     update_db
-    @users = User.all
+    @users = User.all.send(session[:test])
     respond_to do |format|
-      format.js
+      format.js {}
     end
   end
 end
-
-  	# @object = JSON.load(open("https://www.eventbriteapi.com/v3/events/***REMOVED***/attendees/?token=54X654CD2AJ3XPAPM45B"))
-  	# @attend = @object["attendees"];
-  	# @attend.each do |test| 
-  	#	user = User.new
-  	#	user.name = test["profile"]["name"]
-  	#	user.email = test["profile"]["email"]
-		# user.save 
-	  # end 
